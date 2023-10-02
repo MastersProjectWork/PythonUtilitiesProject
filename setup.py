ddec6fa661f76a115944b7059847308a660d2fc5
Michael Male <malmike21@gmail.com>
2023-10-02 18:54:07 +0200

    add package setup configurations

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="pythonUtilitiesProject",
+  version="0.1.0",
+  description="Utilities used for the german supermarket web scrapping project",
+  packages= find_packages(),
+  classifiers=[
+        "Programming Language :: Python :: 3",
+  ],
+  python_requires=">=3.6",
+)
+
